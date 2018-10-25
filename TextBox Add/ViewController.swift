0e5fae530e5a5725ffb46e5dc36aa6899c10f734
Git1Sunny <764152162@qq.com>
2018-10-25 11:46:21 +0800

    Initial Commit

---
+++ TextBox Add/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextBox Add
+//
+//  Created by S20171102171 on 2018/10/25.
+//  Copyright © 2018年 武英琦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TextBox AddTests/TextBox_AddTests.swift
@@ -0,0 +1,34 @@
+//
+//  TextBox_AddTests.swift
+//  TextBox AddTests
+//
+//  Created by S20171102171 on 2018/10/25.
+//  Copyright © 2018年 武英琦. All rights reserved.
+//
+
+import XCTest
+@testable import TextBox_Add
+
+class TextBox_AddTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
